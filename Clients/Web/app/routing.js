a834c9d34536ee8f5d4802802f792e8803f4c188
Christian Liebel <christian.liebel@thinktecture.com>
2015-07-14 21:30:21 +0200

    Decided for project structure/added missing files

--- Clients/Web/app/routing.js
+++ Clients/Web/app/routing.js
@@ -10,8 +10,7 @@
     function RoutingConfig($stateProvider, $urlRouterProvider) {
         $stateProvider
             .state('main', getState('main', '/', true))
-            .state('login', getState('login'))
-            .state('apiAccess', getState('apiAccess'))
+            .state('profile', getState('login'))
             .state('tokenReceived', getState('tokenReceived'));
 
         $urlRouterProvider.otherwise('/');
