7ff007c69caad393ccc3d74b5f7bf1514b516ce6
Christian Liebel <christian.liebel@thinktecture.com>
2015-07-16 22:43:15 +0200

    Redirect uri is determined by edition

--- Clients/Web/app/app.js
+++ Clients/Web/app/app.js
@@ -3,17 +3,19 @@
 
     window.app = window.app || { resolver: {} };
 
+    var isApp = !!window.cordova;
+
+    // Für Apps wird die Modal-Methode verwandt, für Webseiten die „reguläre“ Weiterleitung.
+    var redirectUri = isApp ? 'http://localhost:8080/modal.html' : 'https://localhost:8080/#/tokenReceived?x=x&';
+    // Hinweis: ?x=x& ist hier erforderlich, da der OIDC Token Manager aktuell noch keine Weiterleitung
+    // an Seiten mit Routing-Frameworks unterstützt. Dieser Workoround kann entfallen,
+
     window.oidcConfiguration = {
         // client id
         client_id: "angular",
 
         // redirect URI an den token geschickt wird
-        redirect_uri: "http://localhost:8080/modal.html",
-        // Für Redirect-Methode:
-        //redirect_uri: "https://localhost:8080/#/tokenReceived?x=x&",
-        // Hinweis: ?x=x& ist hier erforderlich, da der OIDC Token Manager keine Kenntnis über den
-        // Aufbau von Routen-Parametern für UI-Router besitzt und den Location-Hash starr an allen
-        // Kaufmannsund-Zeichen (&) splittet, und nicht am Fragezeichen
+        redirect_uri: redirectUri,
 
         // URI, auf die nach dem Logout zurückgeleitet wird
         post_logout_redirect_uri: 'http://localhost:8080/',
@@ -25,16 +27,13 @@
         scope: "openid user_data webapi",
 
         // basis-pfad zu identityserver (für discovery dokument)
-        authority: "http://localhost:8081/",
-
-        // zu Demonstrationszwecken: alle Claims speichern/anzeigen
-        filter_protocol_claims: false
+        authority: "http://localhost:8081/"
     };
 
     app.module = angular.module("authDemo", ["ui.router"]);
 
     app.module.constant('webApiBaseUrl', 'https://localhost:44347/');
-    app.module.constant('isApp', false);
+    app.module.constant('isApp', isApp);
 
     app.module.controller('appController', function ($scope) {
         $scope.page = {
